9a68b9364691eb1cd1cfcfe7f68191028c1497ea
DarrenHoAdiputra <darrenho2008@gmail.com>
2025-05-13 07:31:45 +0000

    Submit

---
+++ assignments/greeting.py
@@ -0,0 +1,10 @@
+def greet(name):
+    print(f"Hello, {name}! Welcome to the game.")
+
+# Ask for player's name
+player1 = input("What is the first player's name?")
+player2 = input("What is the second player's name?")
+
+# Greet both players
+greet(player1)
+greet(player2)
---
+++ assignments/monsters (IN PROGRESS).py
@@ -0,0 +1,9 @@
+# Main coding
+def create_monster(order):
+    print(f"Let's create the {order} monster!")
+    name = input("What's the monster's name? ")
+    hp = input("What's the monster's HP? ")
+    attack = input("What's the monster's attack power? ")
+    print(f"Monster created: {name}, HP: {hp}, Attack: {attack}\n")
+
+# Creating two monsters (first and second monster)
